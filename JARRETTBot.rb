46e34b797717f7e59072a3d82dc4394cdb8ffe1b
LindseyB <hazardous.narayan@gmail.com>
2011-07-11 15:20:39 -0400

    Let's actually catch Exceptions where they may happen

--- JARRETTBot.rb
+++ JARRETTBot.rb
@@ -15,13 +15,17 @@ def jarrettify text
 end
 
 while true do 
+  begin
   Twitter.user_timeline("JarrettB", options).each do |status|
     begin
       Twitter.update(jarrettify status[:text])
-    rescue Exception, Twitter::Forbidden, Twitter::BadGateway
+    rescue Exception
       # don't bother attempting to retry, just don't explode
     end
     options[:since_id] = status[:id]
   end
-  sleep 30 
+  sleep 30
+  rescue Exception
+    # don't care, just don't explode
+  end 
 end 
